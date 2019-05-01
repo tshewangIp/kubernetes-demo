9660c701eae85d4ec0fcad11c7e64d86ea6246b8
Tshewang Tenzin <49857574+tshewangIp@users.noreply.github.com>
2019-05-01 14:15:47 +0600

    Update deploy.sh

--- wordpress/deploy.sh
+++ wordpress/deploy.sh
@@ -13,8 +13,11 @@ kubectl create namespace $ns
 # create resources
 kubectl create -n $ns -f wpmysql-pv.yml --validate=false
 kubectl create -n $ns -f wp-pv.yml --validate=false
+#created the secret with base 64 encoder, decode with base 64
 kubectl create -n $ns -f secret-pass.yml --validate=false
 kubectl create -n $ns -f mysql-deployment.yaml --validate=false
+#stable and reliable for service, this will create the services when there is crashes and others
 kubectl create -n $ns -f wordpress-deployment.yaml --validate=false
+#scaling the container, create and delete the container accoriding the requirements
 kubectl create -n $ns -f autoscale-wordpress.yml --validate=false
 
